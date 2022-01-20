4a368fe3305a0be36747f53819bc341d78ccc43e
vanthaotran <97933995+vanthaotran@users.noreply.github.com>
2022-01-20 13:57:19 +0700

    Add files via upload

---
+++ slide 5/b1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace b1
+{
+    delegate void updateNameHandler(string name);
+    class Program
+    {
+        class HocSinh
+        {
+            public event updateNameHandler NameChanged;
+            private string name;
+            public string Name
+            {
+                get => name;
+                set
+                {
+                    name = value;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            HocSinh hs = new HocSinh();
+            hs.NameChanged += Hs_NameChanged;
+            hs.Name = "Vannn";
+            Console.WriteLine("Ten tu class : " + hs.Name);
+
+            hs.Name = "Hanhh";
+            Console.WriteLine("Ten tu class : " + hs.Name);
+        }
+
+        private static void Hs_NameChanged(string name)
+        {
+            Console.WriteLine("Ten moi : " + name);
+        }
+    }
+}
---
+++ slide 5/baigiang/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace baigiang
+{
+    class Program
+    {
+        // tao delegate
+        delegate int MyDelegate(string s);
+        // methods cho delegate
+        static int ConvertStringToInt(string stringValue)
+        {
+            int valueInt = 0;
+            Int32.TryParse(stringValue, out valueInt);
+            Console.WriteLine("Da ep kieu thanh cong");
+            return valueInt;
+        }
+        static void Main(string[] args)
+        {
+            MyDelegate convertToInt = new MyDelegate(ConvertStringToInt);
+            //string numberStr = "35";
+            int valueConverted = convertToInt("35");
+            Console.WriteLine("Gia tri da convert thanh int: " + valueConverted);
+            //Console.ReadLine();
+        }
+    }
+}
